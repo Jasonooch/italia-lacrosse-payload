0bbc7e784c6d90468ca00aa1f1061ddaa5a22526
Jasonooch <jasonorlando14@gmail.com>
2026-02-10 21:05:24 -0500

    feat: add favicon and enhance navigation styling
    Added a favicon to the configuration and updated the navigation group styles to use a flex layout with a column direction and spacing between items.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -44,6 +44,9 @@ export default buildConfig({
     importMap: {
       baseDir: path.resolve(dirname),
     },
+    meta: {
+      favicon: '/logo-lt.png',
+    },
     components: {
       Nav: './components/admin/Nav/Nav.tsx#default',
       graphics: {
